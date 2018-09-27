81bca159ac981e9cad82476814a2121b437e081a
juliamazur <julasia@gmail.com>
2018-09-27 21:24:28 +0200

    init

---
+++ src/task.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  margin-bottom: 8px;
+  padding: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  background-color: ${props =>  (props.isDragging ? 'lightgreen' : 'white')};
+`;
+
+class Task extends React.Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            innerRef={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
+
+export default Task;
